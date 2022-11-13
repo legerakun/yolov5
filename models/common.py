7274ce69cb669d67582e07853e25292c36511911
Legera <38213687+legerakun@users.noreply.github.com>
2022-11-13 19:45:51 +0800

    Update common.py
    Signed-off-by: Legera <38213687+legerakun@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -772,7 +772,7 @@ class Detections:
 
             im = Image.fromarray(im.astype(np.uint8)) if isinstance(im, np.ndarray) else im  # from np
             if show:
-                display(im) #if is_notebook() else im.show(self.files[i])
+                display(im) im.show(self.files[i])
             if save:
                 f = self.files[i]
                 im.save(save_dir / f)  # save
